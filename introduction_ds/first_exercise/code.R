42b95a94a70db93c8aa2e81943ab5eeb756faba1
Rennan Guimarães <rennan.guimaraes.rg@outlook.com>
2024-08-11 12:54:52 -0300

    add: code to resolve activity

--- introduction_ds/first_exercise/code.R
+++ introduction_ds/first_exercise/code.R
@@ -3,10 +3,11 @@ library(renv)
 renv::init()
 renv::activate()
 
-data <- read.table("./data/German data file.data", header = FALSE, sep = " ")
-
-summary(data)
-
 install.packages("tidyverse")
 library(tidyverse)
+
+data <- read.table("./data/German data.data", header = FALSE, sep = " ")
+
+data |> 
+  glimpse()
 
